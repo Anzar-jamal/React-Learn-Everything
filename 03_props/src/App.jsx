13e0b7ec1012feac3fce2d5a782079e0d7f398fe
Anzar Jamal <anzar543210@gmail.com>
2025-12-19 11:01:27 +0530

    03_props completed

--- 03_props/src/App.jsx
+++ 03_props/src/App.jsx
@@ -4,8 +4,8 @@ import Card from './components/Card'
 const App = () => {
   return (
     <div className='parent'>
-      <Card user="Anzar" url="https://plus.unsplash.com/premium_photo-1765718915206-f3bcb87444b7?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
-      <Card user="Sarfaraz" url="https://images.unsplash.com/photo-1764336312138-14a5368a6cd3?q=80&w=1400&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+      <Card user="Anzar" url="https://plus.unsplash.com/premium_photo-1765718915206-f3bcb87444b7?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" age={24}/>
+      <Card user="Sarfaraz" url="https://images.unsplash.com/photo-1764336312138-14a5368a6cd3?q=80&w=1400&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" age={26} />
     </div>
   )
 }
--- 03_props/src/components/Card.jsx
+++ 03_props/src/components/Card.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 
 const Card = (props) => {
+
+  console.log(props)
     
   return (
     <div>
         <div className="card">
         <img src={props.url} alt="" />
-        <h1>{props.user}</h1>
+        <h1>{props.user},{props.age}</h1>
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione, placeat!</p>
         <button>View profile</button>
       </div>
